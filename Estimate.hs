15e20c46b7f26ced4c3b43774025e493eb31a21a
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-02-08 18:29:09 +0100

    Renames to clarify intent and better readability.

--- Estimate.hs
+++ Estimate.hs
@@ -10,16 +10,13 @@ data Estimate = Estimate {description :: Task,
                           worst :: Time}
               deriving Show
 data Eta = Eta {task :: Task,
-                estimation :: Time,
+                amount :: Time,
                 delta :: Time}
 
 instance Show Eta where
     show (Eta t e d) = "ETA of " ++ show t ++ ": " ++ show e ++ " +/- " ++ show d
 
 estimate :: Estimate -> Eta
-estimate (Estimate {description = t,
-                    best = a,
-                    normal = m,
-                    worst = b}) = (Eta t e d) 
-    where e = (a + 4 * m + b) / 6
-          d = (b - a) / 6
+estimate estimation = (Eta (description estimation) e d) 
+    where e = ((best estimation) + 4 * (normal estimation) + (worst estimation)) / 6
+          d = ((worst estimation) - (best estimation)) / 6
